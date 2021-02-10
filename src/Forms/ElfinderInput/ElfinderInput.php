66e289ed5aef08a8aba6ebf03bd47b34ca000818
Svatopluk Hastík <s.hastik@gmail.com>
2021-02-10 23:11:55 +0100

    Allow to add custom asset properties (input type text or textarea)

--- src/Forms/ElfinderInput/ElfinderInput.php
+++ src/Forms/ElfinderInput/ElfinderInput.php
@@ -2,21 +2,34 @@
 
 namespace Ngscz\Elfinder\Forms;
 
+use App\Model\Asset\Asset;
+use App\Model\Asset\AssetTable;
 use Nette\Forms\Controls\BaseControl;
+use Nette\Utils\Json;
 use Nette\Utils\Html;
+use Nette\Utils\JsonException;
 
 class ElfinderInput extends BaseControl
 {
+    public const OPTION_FIELDS = 'fields';
+    public const OPTION_LOCALES = 'locales';
+
     /** @var bool $multiple */
     private $multiple = false;
 
     /** @var array $onlyMimes */
     private $onlyMimes = [];
 
-    /** @var mixed */
+    /** @var AssetTable */
     private $assetTable;
 
-    public function __construct($caption = null, $assetTable)
+    /** @var array */
+    protected $values = [];
+
+    /** @var Asset[]  */
+    protected $files = [];
+
+    public function __construct($caption, $assetTable)
     {
         parent::__construct($caption);
         $this->assetTable = $assetTable;
@@ -30,6 +43,9 @@ class ElfinderInput extends BaseControl
         $control->setAttribute('data-multiple', $this->multiple ? '1': '0');
         $control->setAttribute('data-only-mimes', implode(',', $this->onlyMimes));
 
+        $control->setAttribute('data-locales', Json::encode($this->getOption(self::OPTION_LOCALES)));
+        $control->setAttribute('data-fields', Json::encode($this->getOption(self::OPTION_FIELDS)));
+
         $control->setAttribute('data-elfinder', 'true');
         $control->setAttribute('style', 'display: none;');
 
@@ -41,47 +57,42 @@ class ElfinderInput extends BaseControl
     {
         // convert object to array
         if ($value) {
-            $newValue = array();
-            if (is_array($value)) {
-                foreach ($value as $file) {
-                    if (is_object($file)) {
-                        $newValue[] = array(
-                            'hash' => $file->getHash(),
-                            'url' => '/uploads' . $file->getPath(),
-                        );
-                    }
-                }
-            } else if (is_object($value)) {
-                $newValue[] = array(
-                    'hash' => $value->getHash(),
-                    'url' => '/uploads' . $value->getPath(),
-                );
+            if (!is_array($value)) {
+                $value = [$value];
             }
-            if ($newValue) {
+
+            $newValue = [];
+            foreach ($value as $file) {
+                if (is_object($file)) {
+                    $formValue = [
+                        'hash' => $file->getHash(),
+                        'url' => '/uploads' . $file->getPath(),
+                    ];
+                    $newValue[] = $formValue;
+
+                    $this->files[] = $file;
+                }
+            }
+
+            if ($newValue !== []) {
                 $value = $newValue;
             }
         }
 
-
         if (is_array($value)) {
-            $this->value = json_encode($value);
+            $this->value = Json::encode($value);
         } else {
             $this->value = $value;
         }
+
         return $this;
     }
 
     public function getValue()
     {
-        $value = json_decode($this->getRawValue(), true);
-        $values = [];
+        $values = $this->getValues();
 
-        //@todo remove dependency on global container, we should use interface instead of this to return correct values
-        if ($value && count($value)) {
-            foreach ($value as $item) {
-                $values[] = $this->assetTable->getOneByHash($item['hash']);
-            }
-        }
+        $values = array_column($values, 'file');
 
         if ($this->multiple) {
             return $values;
@@ -89,9 +100,49 @@ class ElfinderInput extends BaseControl
             if ($values) {
                 return reset($values);
             }
-            return null;
         }
 
+        return null;
+    }
+
+    public function getValues(): array
+    {
+        if ($this->values === []) {
+
+            if ($this->getRawValue() === null) {
+                return [];
+            }
+
+            $decodedValues = [];
+            $rawValues = Json::decode($this->getRawValue(), 1);
+            if ($rawValues && count($rawValues)) {
+                foreach ($rawValues as $item) {
+                    if (is_string($item)) {
+                        $item = Json::decode($item, 1) ?? null;
+                    }
+
+                    if ($item) {
+                        $decodedValues[] = $item;
+                    }
+                }
+            }
+            $values = [];
+
+            if (isset($decodedValues[0][0])) {
+                $decodedValues = $decodedValues[0];
+            }
+
+            foreach ($decodedValues as $item) {
+                //@todo remove dependency on global container, we should use interface instead of this to return correct values
+                $file = $this->assetTable->getOneByHash($item['hash']);
+
+                $values[] = array_merge($item, ['file' => $file]);
+            }
+
+            $this->values = $values;
+        }
+
+        return $this->values;
     }
 
     public function getRawValue()
